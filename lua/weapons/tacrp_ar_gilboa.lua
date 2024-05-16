e56bfd98b7df84de747ba6fdd5f6ea5d6679bb0a
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2024-05-15 21:01:33 -0400

    Revert "ebullet and zoom now use ATT.Compatibility"
    This reverts commit a7b01e6205fea0ca21f8ccf49635f16e15617f91.

--- lua/weapons/tacrp_ar_gilboa.lua
+++ lua/weapons/tacrp_ar_gilboa.lua
@@ -257,7 +257,7 @@ SWEP.Attachments = {
     },
     [2] = {
         PrintName = "Tactical",
-        Category = {"tactical"},
+        Category = {"tactical", "tactical_zoom", "tactical_ebullet"},
         Bone = "pdw_ROOT",
         WMBone = "Box01",
         AttachSound = "TacRP/weapons/flashlight_on.wav",
